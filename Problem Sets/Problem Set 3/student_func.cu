da6886116d243294290a961fb7f25ed697c6f255
Joseph Kain <joekain@gmail.com>
2016-05-13 12:49:44 -0700

    Extract min/max reduce into a separate function

--- Problem Sets/Problem Set 3/student_func.cu
+++ Problem Sets/Problem Set 3/student_func.cu
@@ -117,19 +117,12 @@ void max_step(const float* const input,
   }
 }
 
-void your_histogram_and_prefixsum(const float* const d_logLuminance,
-                                  unsigned int* const d_cdf,
-                                  float &min_logLum,
-                                  float &max_logLum,
-                                  const size_t numRows,
-                                  const size_t numCols,
-                                  const size_t numBins)
+void reduce_min_max(const float *const d_logLuminance,
+                    float &min_logLum,
+                    float &max_logLum,
+                    const size_t numRows,
+                    const size_t numCols)
 {
-  //TODO
-  /*Here are the steps you need to implement
-    1) find the minimum and maximum value in the input logLuminance channel
-    store in min_logLum and max_logLum */
-
   float *t1, *t2;
 
   size_t initial_size = numRows * numCols;
@@ -174,14 +167,28 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
   checkCudaErrors(cudaMemcpy(&max_logLum, src, sizeof(float), cudaMemcpyDeviceToHost));
   printf("max_logLum = %f\n", max_logLum);
 
+  cudaFree(t1);
+  cudaFree(t2);
+}
+
+void your_histogram_and_prefixsum(const float* const d_logLuminance,
+                                  unsigned int* const d_cdf,
+                                  float &min_logLum,
+                                  float &max_logLum,
+                                  const size_t numRows,
+                                  const size_t numCols,
+                                  const size_t numBins)
+{
+  //TODO
+  /*Here are the steps you need to implement
+    1) find the minimum and maximum value in the input logLuminance channel
+    store in min_logLum and max_logLum */
+  reduce_min_max(d_logLuminance, min_logLum, max_logLum, numRows, numCols);
+
   /*2) subtract them to find the range
     3) generate a histogram of all the values in the logLuminance channel using
        the formula: bin = (lum[i] - lumMin) / lumRange * numBins
     4) Perform an exclusive scan (prefix sum) on the histogram to get
        the cumulative distribution of luminance values (this should go in the
        incoming d_cdf pointer which already has been allocated for you)       */
-  cudaFree(t1);
-  cudaFree(t2);
-
-
 }
